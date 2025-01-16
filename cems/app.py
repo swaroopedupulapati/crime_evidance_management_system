2e0581dbfb6636cb17349a3e784918ee29f84434
swaroopedupulapati <swaroopedupulapati1@gmail.com>
2025-01-16 20:34:22 +0530

    edit case

--- cems/app.py
+++ cems/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, jsonify, flash, send_file
+from flask import Flask, request, render_template, redirect, url_for, jsonify, flash, send_file, Response
 from pymongo import MongoClient
 import gridfs
 from bson.objectid import ObjectId
@@ -257,15 +257,7 @@ def view():
 
     return render_template('view_case.html', case_data=case_data, file_contents=file_contents)
 
-from io import BytesIO
-from flask import send_file, flash, redirect, url_for
-from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
-from reportlab.lib.styles import getSampleStyleSheet
-from bson import ObjectId
-import mimetypes
-import fitz  # PyMuPDF
-  # PyMuPDF for PDF processing
+
 
 @app.route('/download_pdf/<case_number>')
 def download_pdf(case_number):
@@ -350,8 +342,69 @@ def download_pdf(case_number):
 
 
 
+@app.route('/edit_case', methods=['GET', 'POST'])
+def edit_case():
+    if request.method == 'POST':
+        case_number = request.form.get('case_number')
+        labels = request.form.getlist('label[]')
+        texts = request.form.getlist('data_text[]')
+        files = request.files.getlist('data_file[]')
+
+        # Prepare case details
+        case_details = []
+        for i in range(len(labels)):
+            detail = {'label': labels[i], 'data': texts[i] if texts[i] else None}
+            if files[i] and files[i].filename:
+                file_id = fs.put(files[i], filename=files[i].filename)
+                detail['file_id'] = str(file_id)
+            case_details.append(detail)
+
+        # Insert or update the case in MongoDB
+        collection.update_one(
+            {'case_number': case_number},
+            {'$set': {'details': case_details}},
+            upsert=True
+        )
+        return "Case details have been successfully submitted or updated!"
+
+    return render_template('edit_case.html')
 
 
+@app.route('/fetch_case/<case_number>', methods=['GET'])
+def fetch_case(case_number):
+    """Fetch case details by case number."""
+    case = collection.find_one({'case_number': case_number})
+    if case:
+        # Convert ObjectId fields to strings
+        case['_id'] = str(case['_id'])
+        for detail in case['details']:
+            if 'file_id' in detail:
+                detail['file_id'] = str(detail['file_id'])
+                file = fs.get(ObjectId(detail['file_id']))
+                detail['filename'] = file.filename
+        return jsonify(case)
+    return jsonify({'error': 'Case not found'}), 404
+
+@app.route('/downloadd/<file_id>')
+def downloadd(file_id):
+    """Download or render files from GridFS using file_id."""
+    file_data = fs.get(ObjectId(file_id))
+    file_type = file_data.filename.split('.')[-1].lower()
+    content_type = {
+        'pdf': 'application/pdf',
+        'jpg': 'image/jpeg',
+        'jpeg': 'image/jpeg',
+        'png': 'image/png',
+        'gif': 'image/gif',
+        'mp4': 'video/mp4',
+        'webm': 'video/webm',
+        'mp3': 'audio/mpeg',
+        'wav': 'audio/wav',
+        'txt': 'text/plain',
+        'csv': 'text/csv',
+    }.get(file_type, 'application/octet-stream')
+
+    return Response(file_data.read(), content_type=content_type)
 
 
 app.run(debug=True)
